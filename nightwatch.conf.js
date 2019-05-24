d7eb22f8ea6af0fd50826bec698da83a7f23fcd7
bxxstian <seb_faull@hotmail.co.uk>
2019-05-24 15:25:15 +0100

    Add CI with travis

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,30 +1,39 @@
 module.exports = {
-  "src_folders": ["tests"],
-  "page_objects_path": ["page-objects"],
+    "src_folders": ["tests"],
+    "page_objects_path": ["page-objects"],
 
-  "webdriver": {
-    "start_process": true,
-    "server_path": "node_modules/.bin/chromedriver",
-    "port": 9515
-  },
-
-  "test_settings": {
-    "default": {
-      "desiredCapabilities": {
-        "browserName": "chrome"
-      }
-    }
-    /*,Add more  browsers for testing if you like
-    "firefox" : {
-      "desiredCapabilities": {
-        "browserName": "firefox"
-      }
+    "webdriver": {
+      "start_process": true,
+      "server_path": "node_modules/.bin/chromedriver",
+      "port": 9515
     },
-    "safari" : {
-      "desiredCapabilities": {
-        "browserName": "safari"
-      }
+
+    'test_settings': {
+        'default': {
+            'screenshots': {
+                'enabled': true,
+                'on_failure': true,
+                'on_error': true,
+                'path': 'tests_output/screenshots'
+            },
+            'desiredCapabilities': {
+                'browserName': 'chrome',
+                'chromeOptions': {
+                    'args': ['--headless']
+                }
+            }
+        }
+        /*,Add more  browsers for testing if you like
+        "firefox" : {
+          "desiredCapabilities": {
+            "browserName": "firefox"
+          }
+        },
+        "safari" : {
+          "desiredCapabilities": {
+            "browserName": "safari"
+          }
+        }
+        */
     }
-    */
-  }
 };
